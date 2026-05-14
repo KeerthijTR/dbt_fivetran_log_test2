d936db90a177e6de8d443741070235f1d71fd4f6
KeerthijTR <Keerthi.J@thomsonreuters.com>
2026-05-14 16:54:11 +0530

    Update fivetran_platform__schema_changelog.sql

--- models/fivetran_platform__schema_changelog.sql
+++ models/fivetran_platform__schema_changelog.sql
@@ -1,9 +1,43 @@
+{{ config(
+    materialized = 'incremental',
+    unique_key = (
+        'schema_change_key' if (
+            (target.type in ('postgres', 'redshift', 'snowflake', 'sqlserver'))
+            or (target.type=='databricks' and not fivetran_log.is_databricks_all_purpose_cluster())
+            )
+        else None
+    ),
+    partition_by = (
+        {'field': 'schema_change_day', 'data_type': 'date'} if target.type == 'bigquery'
+        else ['schema_change_day'] if fivetran_log.is_databricks_all_purpose_cluster()
+        else None
+    ),
+    cluster_by = (
+        ['schema_change_day'] if target.type == 'snowflake'
+        else None
+    ),
+    incremental_strategy = (
+        'merge' if (target.type=='databricks' and not fivetran_log.is_databricks_all_purpose_cluster())
+        else 'insert_overwrite' if target.type in ('bigquery', 'spark', 'databricks')
+        else 'delete+insert' if fivetran_log.is_incremental_compatible()
+        else None
+    ),
+    file_format = (
+        'delta' if target.type=='databricks'
+        else None
+    )
+) }}
+
 with schema_changes as (
 
     select *
     from {{ ref('stg_fivetran_platform__log') }}
 
     where event_subtype in ('create_table', 'alter_table', 'create_schema', 'change_schema_config')
+
+    {% if is_incremental() %}
+    and cast(created_at as date) > {{ fivetran_log.fivetran_log_lookback(from_date='max(schema_change_day)', interval=7) }}
+    {% endif %}
 ),
 
 connection as (
@@ -42,8 +76,11 @@ final as (
 
         case 
         when event_subtype = 'create_schema' or event_subtype = 'create_table' then {{ fivetran_log.fivetran_log_json_parse(string='message_data', string_path=['schema']) }}
-        else null end as schema_name
-    
+        else null end as schema_name,
+
+        {{ dbt_utils.generate_surrogate_key(['connection_id', 'created_at', 'event_subtype', 'message_data']) }} as schema_change_key,
+        cast({{ dbt.date_trunc('day', 'created_at') }} as date) as schema_change_day
+
     from add_connection_info
 )
 
